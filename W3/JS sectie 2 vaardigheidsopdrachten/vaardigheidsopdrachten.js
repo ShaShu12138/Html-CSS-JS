f52df757d869dd0fadeca5db66071ba2eae1c431
Ze Jing <zejing2001@hotmail.com>
2022-09-08 21:24:54 +0200

    JS sectie 2 vaardigheidsopdrachten

---
+++ W3/JS sectie 2 vaardigheidsopdrachten/vaardigheidsopdrachten.js
@@ -0,0 +1,174 @@
+//video 6-16
+/*
+//1.
+let schoolNow = "ROC Almere";
+let schoolBefore = "Zhuhaishi Yishu Gaoji Zhongxue";
+let diploma = "Havo";
+console.log(schoolNow,schoolBefore,diploma);
+
+//2.
+let age = 21;
+console.log(age);
+
+//11.
+let now = 2022;
+let brithYear = 2001;
+let myAge = now - brithYear;
+
+//12.
+console.log(myAge);
+
+//13.
+console.log(myAge*2);
+
+//15.
+let street = "Sabapier ";
+let number = 2;
+console.log(street+number);
+
+//16.
+let numberOne = 12;
+numberOne = 15;
+console.log(numberOne);
+
+//17.
+let numberOne = 12;
+console.log(numberOne*15);
+
+//18.
+let numberOne = 12;
+console.log(numberOne++);
+
+//19.
+let numberOne = 12;
+console.log(numberOne--);
+
+//20.
+console.log((183*3+178)/4);
+
+//21.
+console.log((153*3+192)/4);
+
+//22.
+console.log(Math.sqrt(93/28.6));
+
+//23.
+console.log(1.88**2*28.6);
+
+
+//video 17-19
+
+//1.
+let firstName = "Ze";
+let lastName = "Jing";
+let age = 21;
+console.log(firstName+lastName+age);
+
+//2.
+console.log(`Mijn naam is ${firstName}`);
+
+//3.
+console.log(`Mijn volledige naam is ${firstName} ${lastName}`);
+
+//4.
+let jaar = 2056;
+let brithYear = 2001;
+console.log(`In ${jaar} ben ik ${jaar-birthYear} jaar oud`);
+
+//5.
+console.log(`Mijn naam is ${firstName} en ik ben ${age} jaar oud. Daarnaast is mijn familienaam ${lastName}`);
+
+//6.
+let regen = true;
+if(regen){
+    console.log("paraplue");
+}else{
+    console.log("Sunglasses");
+}
+
+//7.
+let grassYellow = true;
+if(grassYellow){
+    console.log("Water");
+}else{
+    console.log("nothing");
+}
+
+//8.
+let winter = true;
+if(winter){
+    console.log("warm");
+}else{
+    console.log("dance");
+}
+
+//9.
+let regen = true;
+let groteRegen = false;
+if (groteRegen){
+    console.log("regenpak");
+}else if(regen){
+    console.log("paraplu");
+}else{
+    console.log("zonnebril");
+}
+
+
+//video 20-25
+
+//1.
+console.log(x+y-z);
+
+//2.
+console.log(z-y-x);
+
+//3.
+console.log(z+String(x-y));
+
+//4.
+console.log(y+z+x);
+
+//5.
+console.log(x+String(z*2+y*2));
+
+//6.
+let regen = true;
+if(regen){
+    console.log("paraplu");
+}
+
+//7.
+let sun = false;
+if(regen){
+    console.log("paraplu");
+}else if(sun){
+    console.log("zonnebril");
+}else{
+    console.log("humeur");
+}
+
+//8.
+if(regen){
+    console.log("paraplu");
+}else if(sun){
+    "zonnebril"
+}
+
+//9.
+let day = Maanday;
+switch (day) {
+    case Maanday:
+        console.log("kickboksen");
+        break;
+    case Dinsdag:
+        console.log("boksen");
+        break;
+    case Donderdag:
+    case Vrijdag:
+        console.log("BJJ");
+        break;
+    default:
+        console.log("patat");
+        break;
+}
+*/
